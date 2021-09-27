9c4c4f0c60f96cbde20218a5544c99c791edcd98
soflarity <55975006+soflarity@users.noreply.github.com>
2021-09-27 11:01:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+
+  function getLockers() {
+    const url = 'http://localhost:5000/api/search'
+    fetch()
+  }
+
+  function handleSearch(event) {
+    let searchQuery = event.target.value;
+
+  }
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
